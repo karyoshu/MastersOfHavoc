3890cb897bcf75bfd1d6629f978c25478eb1e8da
Himanshu <himanshu.m@v2g.io>
2017-06-10 19:27:32 +0530

    art assets demo scene setup A* pathfinding

---
+++ Assets/EnemyMovementController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMovementController : MonoBehaviour {
+
+    public Vector3 targetPosition;
+    
+	// Use this for initialization
+	void Start () {
+        Seeker seeker = GetComponent<Seeker>();
+        seeker.StartPath(transform.position, targetPosition, OnPathComplete);	
+	}
+	
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Reached destination");
+    }
+}
